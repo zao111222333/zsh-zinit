025f6a50680a6db22d5e23304e29a36e13071834
junzhuo <junzhuo22@g.ucla.edu>
2023-09-28 20:11:04 +0800

    update

--- zsh/zsh.d/p10k.zsh
+++ zsh/zsh.d/p10k.zsh
@@ -31,8 +31,8 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     # os_icon               # os identifier
-    vcs                     # git status
     prompt_char             # prompt symbol
+    vcs                     # git status
     dir                     # current directory
   )
 
